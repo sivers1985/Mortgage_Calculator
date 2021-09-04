5a23bf60097e86758487ccdfd9e7081bb01b1d87
Sean Ivers <sivers1985@yahoo.com>
2021-09-04 13:05:23 -0400

    Added calculate_debt_schedule method

--- Mortgage_Calculator.py
+++ Mortgage_Calculator.py
@@ -3,32 +3,44 @@
 
 class MortgageCalculator:
 
-    def calculate_monthly_percentage(self, yearly_percentage_rate):
+    def __init__(self, yearly_percentage_rate, loan_term, principal):
+        self.yearly_percentage_rate = yearly_percentage_rate
+        self.loan_term = loan_term
+        self.principal = principal
+        self.monthly_percentage_rate = self.yearly_percentage_rate / 12
+        self.number_monthly_payments = self.loan_term * 12
+
+    def monthly_percentage(self):
 
         # Monthly percentage rate
-        monthly_percentage_rate = yearly_percentage_rate / 12
+        return "Your monthly percentage rate = " + "{:.3f}".format(self.monthly_percentage_rate)
 
-        return monthly_percentage_rate
-
-    def calculate_number_payments(self, loan_term):
+    def number_payments(self):
         
         # Number of monthly payments
-        number_monthly_payments = loan_term * 12
+        return "Your number of payments = " + str(self.number_monthly_payments)
 
-        return number_monthly_payments
-
-    def calculate_monthly_payment(self, monthly_percentage_rate, principal, number_monthly_payments):
+    def calculate_monthly_payment(self):
     
         # Calculate monthly payment
-        if monthly_percentage_rate != 0:
-            monthly_payment = ((monthly_percentage_rate * principal) / (1 - (1 + monthly_percentage_rate) ** -number_monthly_payments))
+        if self.monthly_percentage_rate != 0:
+            monthly_payment = ((self.monthly_percentage_rate * self.principal) / (1 - (1 + self.monthly_percentage_rate) ** -self.number_monthly_payments))
         else:
-            monthly_payment = principal / number_monthly_payments
+            monthly_payment = self.principal / self.number_monthly_payments
 
         return "Your monthly payment = $" + "{:.2f}".format(monthly_payment)
 
-    def calculate_amount_owed(self):
-        pass
+    def calculate_debt_schedule(self):
+        for i in range(self.number_monthly_payments):
+            p = self.principal
+            x = (1 + self.monthly_percentage_rate)
+            # Polynomial of x
+            pn_x = ((x ** i - 1) / (x - 1))
+            # Monthly payment
+            c = ((self.monthly_percentage_rate * self.principal) / (1 - (1 + self.monthly_percentage_rate) ** -self.number_monthly_payments))
+            amount_owed = x ** i * p - pn_x * c
+            print("Amount owed at month {} = {:.2f}".format(i + 1, amount_owed))
+            i += 1
 
 def run_calculator():
 
@@ -37,8 +49,6 @@ def run_calculator():
     print("Enter 2 to exit")
     
     choice = int(input("Enter your choice: "))
-
-    mortgage1 = MortgageCalculator()
 
     while choice != 2:
 
@@ -52,8 +62,18 @@ def run_calculator():
         principal = float(input("What is the amount borrowed? $"))
         print("----------------------------------------")
 
-        print(mortgage1.calculate_monthly_payment(mortgage1.calculate_monthly_percentage(yearly_percentage_rate), principal, 
-        mortgage1.calculate_number_payments(loan_term)))
+        mortgage1 = MortgageCalculator(yearly_percentage_rate, loan_term, principal)
+
+        print(mortgage1.number_payments())
+        print("----------------------------------------")
+
+        print(mortgage1.monthly_percentage())
+        print("----------------------------------------")
+
+        print(mortgage1.calculate_monthly_payment())
+        print("----------------------------------------")
+
+        mortgage1.calculate_debt_schedule()
         print("----------------------------------------")
 
         choice = int(input("Enter 1 to calculate another mortgage payment or 2 to exit: "))
