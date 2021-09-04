c55d7b9a82517a6070e44a9e48790d72c6c1f2f8
Sean Ivers <sivers1985@yahoo.com>
2021-09-03 22:01:39 -0400

    Created run_calculator function

---
+++ Mortgage_Calculator.py
@@ -0,0 +1,24 @@
+# Python Mortgage Calculator
+# First Codecademy project: utilizing Terminal, git, github
+
+
+def get_variables():
+    pass
+
+def calculate_monthly_payment():
+    pass
+
+def calculate_amount_owed():
+    pass
+
+def run_calculator():
+    print("Enter 1 for mortgage calculator")
+    print("Enter 2 to exit")
+    
+    choice = 0
+
+    while choice != 2:
+        
+        choice = int(input("Enter your choice: "))
+
+run_calculator()
