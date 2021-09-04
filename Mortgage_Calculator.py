c211ef80a05a80a8a9fd75d90ec3cea3cbb251ed
Sean Ivers <sivers1985@yahoo.com>
2021-09-03 22:25:37 -0400

    Created get_variables function

--- Mortgage_Calculator.py
+++ Mortgage_Calculator.py
@@ -3,7 +3,14 @@
 
 
 def get_variables():
-    pass
+
+    # Enter the variables for the calculation
+    yearly_percentage_rate = float(input("What is the yearly percentage rate? "))
+    print("----------------------------------------")
+    loan_term = int(input("What is the duration of the mortgage? "))
+    print("----------------------------------------")
+    principal = float(input("What is the amount borrowed? $"))
+    print("----------------------------------------")
 
 def calculate_monthly_payment():
     pass
@@ -12,13 +19,19 @@ def calculate_amount_owed():
     pass
 
 def run_calculator():
+
+    # Choice to keep running or exit
     print("Enter 1 for mortgage calculator")
     print("Enter 2 to exit")
     
-    choice = 0
+    choice = int(input("Enter your choice: "))
 
     while choice != 2:
+
+        get_variables()
+
+        choice = int(input("Enter 1 to calculate another mortgage payment or 2 to exit: "))
+        print("----------------------------------------")
         
-        choice = int(input("Enter your choice: "))
 
 run_calculator()
